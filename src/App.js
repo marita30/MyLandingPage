eb6767c4d68e43621e702bee0fd6ce28c69e595f
marita30 <marita.jose.ch@gmail.com>
2020-06-22 14:08:14 -0400

    Conectando nuestro PageBuilder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import PageBuilder from './containers/PageBuilder';
 
 function App() {
   return (
     <div className="App">
+      <PageBuilder />
       
     </div>
   );
