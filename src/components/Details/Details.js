94ab923b9252f52935566f4a82f560559bc09ed3
marita30 <marita.jose.ch@gmail.com>
2020-06-22 16:51:01 -0400

    AGregndo NavigationItem y NavigationItems y Details donde son mis tres componentes para crear el dise;o establecido, agregando el nav en el navigationItem y Items , y details.js agregando configuracion del componente ue llevara title etc

---
+++ src/components/Details/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const details = (props) => {
+    return(
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+export default details;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './NavigationItems.css';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}> 
+        <NavigationItem link="/" exact>Home </NavigationItem>
+        <NavigationItem link="/about-me">About Me</NavigationItem>
+        <NavigationItem link="/experience"> Experience </NavigationItem>
+        <NavigationItem link="/my_hobbies"> My Hobbies </NavigationItem>
+        <NavigationItem link="/my_family"> My Family </NavigationItem> 
+       
+    </ul>
+
+)
+
+export default navigationItems;
