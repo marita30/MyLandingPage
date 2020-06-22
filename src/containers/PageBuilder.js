bc6356cb9f0a1ba8c298a7826295ba8fd5ec1f87
marita30 <marita.jose.ch@gmail.com>
2020-06-22 16:54:56 -0400

    agregando el componentDidMOunt, el willReeiveProps y creamos un switch case para que se vaya cambiando la informacion por medio del pathname

--- src/containers/PageBuilder.js
+++ src/containers/PageBuilder.js
@@ -2,12 +2,47 @@ import React, { Component } from 'react';
 
 /* Importando css */
 import classes from './PageBuilder.css'
-
+import NavigationItems from '../components/Navigation/NavigationItems/NavigationItems';
+import Details from '../components/Details/Details'
 class PageBuilder extends Component {
+    state = {
+        details: null
+    }
+
+    componentDidMount() {
+        this.setView(this.props.location.pathname)
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        this.setView(nextProps.location.pathname)
+    }
+
+    setView = (pathname) => {
+        switch (pathname) {
+            case('/'):
+                this.setState({
+                    details: <Details title='home'/>
+                })
+                break;
+            case('/about-me'):
+                this.setState({
+                    details: <Details title='MJ'/>
+                })
+                break;
+            default:
+                break;
+        }
+    }
     render(){
         return(
             <div className={classes.PageBuilder}>
-                <p> Prueba</p>
+                
+                <nav>
+                     < NavigationItems />
+                </nav>
+                {this.state.details}
+               
+       
                 
             </div>
         )
