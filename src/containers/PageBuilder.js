db9fb376989cc827d38d3dc967d31ac988966e42
marita30 <marita.jose.ch@gmail.com>
2020-06-22 14:07:55 -0400

    Agregando el container PageBuilder con su css, para la pagina principal de nuestro landing

---
+++ src/containers/PageBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+/* Importando css */
+import classes from './PageBuilder.css'
+
+class PageBuilder extends Component {
+    render(){
+        return(
+            <div className={classes.PageBuilder}>
+                <p> Prueba</p>
+                
+            </div>
+        )
+    }
+
+};
+
+export default PageBuilder;
