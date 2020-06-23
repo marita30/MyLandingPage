503483bac208d18197a6b8209433717cb3b337a1
marita30 <marita.jose.ch@gmail.com>
2020-06-23 16:36:26 -0400

    Agregando mas condiciones al switch case

--- src/containers/PageBuilder.js
+++ src/containers/PageBuilder.js
@@ -4,18 +4,25 @@ import React, { Component } from 'react';
 import classes from './PageBuilder.css'
 import NavigationItems from '../components/Navigation/NavigationItems/NavigationItems';
 import Details from '../components/Details/Details'
+
+
 class PageBuilder extends Component {
+
     state = {
+
         details: null
     }
 
+
     componentDidMount() {
         this.setView(this.props.location.pathname)
-    }
+    };
+
 
     UNSAFE_componentWillReceiveProps(nextProps) {
         this.setView(nextProps.location.pathname)
-    }
+    };
+
 
     setView = (pathname) => {
         switch (pathname) {
@@ -25,26 +32,52 @@ class PageBuilder extends Component {
                 })
                 break;
             case('/about-me'):
+                this.onAboutMe()
+                break;
+            case('/experience'):
                 this.setState({
-                    details: <Details title='MJ'/>
+                    details: <Details title='hello'/>
+                })
+                break;
+            case('/my-hobbies'):
+                this.setState({
+                    details: <Details title='Music'/>
+                })
+                break;
+            case('/my-family'):
+                this.setState({
+                    details: <Details title='loca'/>
                 })
                 break;
             default:
-                break;
+               this.state.details = null;
+            
         }
     }
+
+    onAboutMe = () => {
+        this.setState({
+            details: <Details title='Maria'/>
+        })
+    }
+
     render(){
         return(
-            <div className={classes.PageBuilder}>
-                
-                <nav>
-                     < NavigationItems />
-                </nav>
-                {this.state.details}
+            
+            <div className={classes.somepagewrapper}>
+                    
+                <div>
+                     <nav>
+                        < NavigationItems />
+                    </nav>
+                </div>
+            
+                 <div>
+                    {this.state.details}
+                </div>
+                                     
                
-       
-                
-            </div>
+                </div>
         )
     }
 
